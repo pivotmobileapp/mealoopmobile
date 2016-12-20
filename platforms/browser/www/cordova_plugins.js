fd0d7e4b57f37b2da6f3d25b957a4db86044822b
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2016-12-20 17:35:06 -0400

    2121

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova.plugins.diagnostic": "3.3.2"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/browser/www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={	
+	'ApiUrl' : "http://mealoop.com/mobileapp/api",
+	'DialogDefaultTitle' : "mealoop",
+	'pushNotificationSenderid' : "1039967975155",
+	'facebookAppId' : "1785452871717052",
+	'APIHasKey' : "fed7b441b349bae8f146711fbd215e90"
+};
