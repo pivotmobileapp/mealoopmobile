182bbcbb89800856e0f62ce7290d73f1c33b8ce8
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-02-08 22:50:55 -0400

    ihk

--- www/a/js/config.js
+++
@@ -1,6 +0,0 @@
-angular.module('your_app_name.config', [])
-.constant('WORDPRESS_API_URL', 'http://mealoop.com/main/wp-json/wp/v2/')
-.constant('WORDPRESS_PUSH_URL', 'https://wordpress.startapplabs.com/blog/push/')
-.constant('GCM_SENDER_ID', '574597432927')
-
-;
