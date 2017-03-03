fe68750849a8aa39a8bcc39ac482f3b44f947912
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2017-03-03 09:22:19 -0400

    new push options

--- www/js/app.js
+++ www/js/app.js
@@ -32,14 +32,28 @@ var track_order_map_interval;
 var drag_marker_bounce=1;
 var  _cl_count=0
 
-document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, 
+						  function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("93adf77f-4d3e-4b2a-aea9-3772189f3819")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+  
+  // Call syncHashedEmail anywhere in your app if you have the user's email.
+  // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+  // window.plugins.OneSignal.syncHashedEmail(userEmail);
+},
+						  
+						  false);
 
 function onDeviceReady() {  
-
-
-
-
-
 
 	getCurrentLocation();
 
